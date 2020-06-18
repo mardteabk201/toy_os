6f060f4165548f68f67fee80b0525bc4964f2e81
Liu Yibin <jiulong@linux.alibaba.com>
2020-06-18 19:25:54 +0800

    Format code

--- src/fork.c
+++ src/fork.c
@@ -43,28 +43,6 @@ int copy_process(unsigned long clone_flags, unsigned long fn, unsigned long arg)
 	return pid;
 }
 
-/* 这个函数的实现有点技巧 */
-/* 这个进程刚开始创建时，在ret_from_fork里，sp是0x50000ef0 */
-/* 它知道，自己最终回回到ret_from_fork，并且sp会回到0x50000ef0 */
-/* 所以，pt_regs就是0x50000ef0，然后填充好pt_regs */
-/* 到时候直接执行ret_to_usr里的kernel_exit */
-/* 就可以配置好，sp_el0和esr_el1 */
-int move_to_user_mode(unsigned long start, unsigned long size, unsigned long pc)
-{
-	struct pt_regs *regs = task_pt_regs(current);
-
-	regs->pstate = PSR_MODE_EL0t;
-	regs->pc = pc;
-	regs->sp = 2 * PAGE_SIZE;
-
-	unsigned long code_page = allocate_user_page(current, 0);
-
-	memcpy(code_page, start, size);
-	set_pgd(current->mm.pgd);
-
-	return 0;
-}
-
 struct pt_regs * task_pt_regs(struct task_struct *tsk)
 {
 	unsigned long p = (unsigned long)tsk + THREAD_SIZE - sizeof(struct pt_regs);
--- src/kernel.c
+++ src/kernel.c
@@ -10,8 +10,31 @@
 #include "sched.h"
 #include "sys.h"
 #include "user.h"
+#include "mm.h"
 
-void kernel_process()
+/* 这个函数的实现有点技巧 */
+/* 这个进程刚开始创建时，在ret_from_fork里，sp是0x50000ef0 */
+/* 它知道，自己最终回回到ret_from_fork，并且sp会回到0x50000ef0 */
+/* 所以，pt_regs就是0x50000ef0，然后填充好pt_regs */
+/* 到时候直接执行ret_to_usr里的kernel_exit */
+/* 就可以配置好，sp_el0和esr_el1 */
+int move_to_user_mode(unsigned long start, unsigned long size, unsigned long pc)
+{
+	struct pt_regs *regs = task_pt_regs(current);
+
+	regs->pstate = PSR_MODE_EL0t;
+	regs->pc = pc;
+	regs->sp = 2 * PAGE_SIZE;
+
+	unsigned long code_page = allocate_user_page(current, 0);
+
+	memcpy(code_page, start, size);
+	set_pgd(current->mm.pgd);
+
+	return 0;
+}
+
+void temporary_process()
 {
 	unsigned long begin = (unsigned long)&user_begin;
 	unsigned long end = (unsigned long)&user_end;
@@ -34,7 +57,7 @@ void kernel_main(void)
 	timer_init();
 	enable_irq();
 
-	copy_process(PF_KTHREAD, (unsigned long)&kernel_process, 0);
+	copy_process(PF_KTHREAD, (unsigned long)&temporary_process, 0);
 
 	while (1)
 		schedule();
