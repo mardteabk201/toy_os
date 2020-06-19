8fe0c7e7eb07fd5cf630cfe869588a6215605816
Liu Yibin <jiulong@linux.alibaba.com>
2020-06-19 09:52:52 +0800

    7

--- src/gic_v3.c
+++ src/gic_v3.c
@@ -3,34 +3,7 @@
 #include "exception.h"
 #include "gic_v3.h"
 
-/* Initialize GIC Controller */
-static void init_gicc(void)
-{
-	uint32_t pending_irq;
-
-	/* Disable CPU interface */
-	*REG_GIC_GICC_CTLR = GICC_CTLR_DISABLE;
-
-	/* Set the priority level as the lowest priority */
-	/* Note: Higher priority corresponds to a lower Priority field value in the GIC_PMR.
-	 * In addition to this, writing 255 to the GICC_PMR always sets it to the
-	 * largest supported priority field value.
-	 */
-	*REG_GIC_GICC_PMR = GICC_PMR_PRIO_MIN;
-
-	/* Handle all of interrupts in a single group */
-	*REG_GIC_GICC_BPR = GICC_BPR_NO_GROUP;
-
-	/* Clear all of the active interrupts */
-	for (pending_irq = (*REG_GIC_GICC_IAR & GICC_IAR_INTR_IDMASK);
-	     (pending_irq != GICC_IAR_SPURIOUS_INTR);
-	     pending_irq = (*REG_GIC_GICC_IAR & GICC_IAR_INTR_IDMASK))
-		* REG_GIC_GICC_EOIR = *REG_GIC_GICC_IAR;
-
-	/* Enable CPU interface */
-	*REG_GIC_GICC_CTLR = GICC_CTLR_ENABLE;
-}
-
+/* Initialize GIC Distributor */
 static void init_gicd(void)
 {
 	int32_t	i, regs_nr;
@@ -70,6 +43,34 @@ static void init_gicd(void)
 	*REG_GIC_GICD_CTLR = GIC_GICD_CTLR_ENABLE;
 }
 
+/* Initialize GIC Controller */
+static void init_gicc(void)
+{
+	uint32_t pending_irq;
+
+	/* Disable CPU interface */
+	*REG_GIC_GICC_CTLR = GICC_CTLR_DISABLE;
+
+	/* Set the priority level as the lowest priority */
+	/* Note: Higher priority corresponds to a lower Priority field value in the GIC_PMR.
+	 * In addition to this, writing 255 to the GICC_PMR always sets it to the
+	 * largest supported priority field value.
+	 */
+	*REG_GIC_GICC_PMR = GICC_PMR_PRIO_MIN;
+
+	/* Handle all of interrupts in a single group */
+	*REG_GIC_GICC_BPR = GICC_BPR_NO_GROUP;
+
+	/* Clear all of the active interrupts */
+	for (pending_irq = (*REG_GIC_GICC_IAR & GICC_IAR_INTR_IDMASK);
+	     (pending_irq != GICC_IAR_SPURIOUS_INTR);
+	     pending_irq = (*REG_GIC_GICC_IAR & GICC_IAR_INTR_IDMASK))
+		* REG_GIC_GICC_EOIR = *REG_GIC_GICC_IAR;
+
+	/* Enable CPU interface */
+	*REG_GIC_GICC_CTLR = GICC_CTLR_ENABLE;
+}
+
 /** Disable IRQ
     @param[in] irq IRQ number
  */
@@ -88,27 +89,38 @@ void gicd_enable_int(irq_no irq)
 	    1U << (irq % GIC_GICD_ISENABLER_PER_REG);
 }
 
-/** Clear a pending interrupt
-    @param[in] irq IRQ number
+/** Configure IRQ
+    @param[in] irq     IRQ number
+    @param[in] config  Configuration value for GICD_ICFGR
  */
-void gicd_clear_pending(irq_no irq)
+static void gicd_config(irq_no irq, unsigned int config)
 {
-	*REG_GIC_GICD_ICPENDR((irq / GIC_GICD_ICPENDR_PER_REG)) =
-	    1U << (irq % GIC_GICD_ICPENDR_PER_REG);
+	uint32_t	shift;
+	uint32_t	  reg;
+
+	shift = (irq % GIC_GICD_ICFGR_PER_REG) * GIC_GICD_ICFGR_SIZE_PER_REG; /* GICD_ICFGR has 16 fields, each field has 2bits. */
+
+	reg = *REG_GIC_GICD_ICFGR(irq / GIC_GICD_ICFGR_PER_REG);
+
+	reg &= ~(((uint32_t)(0x03)) << shift);      /* Clear the field */
+	reg |= (((uint32_t)config) << shift);      /* Set the value to the field correponding to irq */
+	*REG_GIC_GICD_ICFGR(irq / GIC_GICD_ICFGR_PER_REG) = reg;
 }
 
-
-/** Probe pending interrupt
-    @param[in] irq IRQ number
+/** Set an interrupt priority
+    @param[in] irq  IRQ number
+    @param[in] prio Interrupt priority in Arm specific expression
  */
-static int gicd_probe_pending(irq_no irq)
+static void gicd_set_priority(irq_no irq, uint32_t prio)
 {
-	int is_pending;
+	uint32_t  shift;
+	uint32_t    reg;
 
-	is_pending = (*REG_GIC_GICD_ISPENDR((irq / GIC_GICD_ISPENDR_PER_REG)) &
-	              (1U << (irq % GIC_GICD_ISPENDR_PER_REG)));
-
-	return (is_pending != 0);
+	shift = (irq % GIC_GICD_IPRIORITY_PER_REG) * GIC_GICD_IPRIORITY_SIZE_PER_REG;
+	reg = *REG_GIC_GICD_IPRIORITYR(irq / GIC_GICD_IPRIORITY_PER_REG);
+	reg &= ~(((uint32_t)(0xff)) << shift);
+	reg |= (prio << shift);
+	*REG_GIC_GICD_IPRIORITYR(irq / GIC_GICD_IPRIORITY_PER_REG) = reg;
 }
 
 /** Set an interrupt target processor
@@ -132,38 +144,13 @@ static void gicd_set_target(irq_no irq, uint32_t p)
 	*REG_GIC_GICD_ITARGETSR(irq / GIC_GICD_ITARGETSR_PER_REG) = reg;
 }
 
-/** Set an interrupt priority
-    @param[in] irq  IRQ number
-    @param[in] prio Interrupt priority in Arm specific expression
+/** Clear a pending interrupt
+    @param[in] irq IRQ number
  */
-static void gicd_set_priority(irq_no irq, uint32_t prio)
+void gicd_clear_pending(irq_no irq)
 {
-	uint32_t  shift;
-	uint32_t    reg;
-
-	shift = (irq % GIC_GICD_IPRIORITY_PER_REG) * GIC_GICD_IPRIORITY_SIZE_PER_REG;
-	reg = *REG_GIC_GICD_IPRIORITYR(irq / GIC_GICD_IPRIORITY_PER_REG);
-	reg &= ~(((uint32_t)(0xff)) << shift);
-	reg |= (prio << shift);
-	*REG_GIC_GICD_IPRIORITYR(irq / GIC_GICD_IPRIORITY_PER_REG) = reg;
-}
-
-/** Configure IRQ
-    @param[in] irq     IRQ number
-    @param[in] config  Configuration value for GICD_ICFGR
- */
-static void gicd_config(irq_no irq, unsigned int config)
-{
-	uint32_t	shift;
-	uint32_t	  reg;
-
-	shift = (irq % GIC_GICD_ICFGR_PER_REG) * GIC_GICD_ICFGR_SIZE_PER_REG; /* GICD_ICFGR has 16 fields, each field has 2bits. */
-
-	reg = *REG_GIC_GICD_ICFGR(irq / GIC_GICD_ICFGR_PER_REG);
-
-	reg &= ~(((uint32_t)(0x03)) << shift);      /* Clear the field */
-	reg |= (((uint32_t)config) << shift);      /* Set the value to the field correponding to irq */
-	*REG_GIC_GICD_ICFGR(irq / GIC_GICD_ICFGR_PER_REG) = reg;
+	*REG_GIC_GICD_ICPENDR((irq / GIC_GICD_ICPENDR_PER_REG)) =
+	    1U << (irq % GIC_GICD_ICPENDR_PER_REG);
 }
 
 /** Send End of Interrupt to IRQ line for GIC
@@ -175,23 +162,18 @@ void gic_v3_eoi(irq_no irq)
 	gicd_clear_pending(irq);
 }
 
-/* Initialize GIC IRQ controller */
-/* RyanYao: 2018/07/20
- *    I supppose the current access is security, because GICD_CTLR.DS is 0b0 and
- *    we can access.
+/** Probe pending interrupt
+    @param[in] irq IRQ number
  */
-void gic_v3_initialize(void)
+static int gicd_probe_pending(irq_no irq)
 {
+	int is_pending;
 
-	init_gicd();
-	init_gicc();
-	gicd_config(TIMER_IRQ, GIC_GICD_ICFGR_EDGE); /* No Need */
-	gicd_set_priority(TIMER_IRQ, 0 << GIC_PRI_SHIFT);   /* Set priority */
-	gicd_set_target(TIMER_IRQ, 0x1);  /* processor 0 */  /* No need */
-	gicd_clear_pending(TIMER_IRQ); /* No need */
-	gicd_enable_int(TIMER_IRQ);
+	is_pending = (*REG_GIC_GICD_ISPENDR((irq / GIC_GICD_ISPENDR_PER_REG)) &
+	              (1U << (irq % GIC_GICD_ISPENDR_PER_REG)));
+
+	return (is_pending != 0);
 }
-
 
 /** Find pending IRQ
     @param[in]     exc  An exception frame
@@ -213,4 +195,21 @@ int gic_v3_find_pending_irq(struct _exception_frame *exc __attribute__((unused))
 	rc = IRQ_NOT_FOUND ;
 found:
 	return rc;
+}
+
+/* Initialize GIC IRQ controller */
+/* RyanYao: 2018/07/20
+ *    I supppose the current access is security, because GICD_CTLR.DS is 0b0 and
+ *    we can access.
+ */
+void gic_v3_initialize(void)
+{
+
+	init_gicd();
+	init_gicc();
+	gicd_config(TIMER_IRQ, GIC_GICD_ICFGR_EDGE); /* No Need */
+	gicd_set_priority(TIMER_IRQ, 0 << GIC_PRI_SHIFT);   /* Set priority */
+	gicd_set_target(TIMER_IRQ, 0x1);  /* processor 0 */  /* No need */
+	gicd_clear_pending(TIMER_IRQ); /* No need */
+	gicd_enable_int(TIMER_IRQ);
 }
